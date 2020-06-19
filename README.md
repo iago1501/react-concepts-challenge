b5dc9e749e771e04b5cd44db42e12529939960f6
Iago Espinoza <iago1501@gmail.com>
2020-06-19 00:50:52 -0300

    Desafio finalizado 100% testes

---
+++ README.md
@@ -0,0 +1,24 @@
+Projeto criado para o desafio do módulo 1 da rocketseat, conceitos de React
+
+## Scripts disponíveis
+
+Para rodar o projeto basta executar
+
+### `yarn start`
+
+Roda a aplicação em modo de desenvolvimento.<br />
+Abra [http://localhost:3000](http://localhost:3000) para ver o projeto no browser.
+
+### `yarn test`
+
+Para executar os testes necessários para a aplicação ser considerada criada com sucesso
+
+## Fundamentos
+
+Neste projeto foram colocados em prática os conhecimentos adquiridos no módulo 1 do curso de React,
+utilizando a api criada no módulo de node para repositórios no Github, com as seguintes tecnologias: 
+
+- Hooks (useState, useEffect);
+- Axios;
+- Async/Await;
+- Promisses;
--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((resp) => setRepositories(resp.data));
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    api
+      .post("repositories", {
+        title: `Repositório ${repositories.length + 1}`,
+        url: "http://github.com/iago1501",
+        techs: ["Node.js", "Express"],
+      })
+      .then((resp) => setRepositories([...repositories, resp.data]));
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api
+      .delete(`repositories/${id}`)
+      .then(
+        setRepositories(
+          repositories.filter((repositorie) => repositorie.id !== id)
+        )
+      );
   }
-
   return (
     <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+      <ul data-testid="repository-list">        
+        {repositories.map((repositorie) => (
+          <li>
+            <a href={repositorie.url}>{repositorie.title}</a>
+            <button onClick={() => handleRemoveRepository(repositorie.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
